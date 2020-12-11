f426cbd6c49fd62c2ef776bc7e98e9a80224c540
Ryan Miville <ryan.miville@terminus.com>
2020-12-11 09:11:37 -0500

    writer

--- s3/bucket.go
+++ s3/bucket.go
@@ -3,7 +3,6 @@ package s3
 import (
 	"context"
 	"io"
-	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
 	awss3 "github.com/aws/aws-sdk-go/service/s3"
@@ -28,8 +27,8 @@ func (b *Bucket) NewReader(ctx context.Context, key string) (io.ReadCloser, erro
 	return resp.Body, nil
 }
 
-func (b *Bucket) NewWriter(ctx context.Context, key string) (io.WriteCloser, error) {
-	send := func(r io.Reader) error {
+func (b *Bucket) NewWriter(ctx context.Context, key string) io.WriteCloser {
+	upload := func(r io.Reader) error {
 		in := &s3manager.UploadInput{
 			Bucket: aws.String(b.name),
 			Key:    aws.String(key),
@@ -38,29 +37,22 @@ func (b *Bucket) NewWriter(ctx context.Context, key string) (io.WriteCloser, err
 		_, err := b.client.UploadWithContext(ctx, in)
 		return err
 	}
-	return &writer{
-		send: send,
-	}, nil
+	pr, pw := io.Pipe()
+	w := &writer{
+		w:     pw,
+		donec: make(chan struct{}),
+	}
+	w.pipe(pr, upload)
+	return w
 }
 
 type writer struct {
 	w     *io.PipeWriter
-	send  func(r io.Reader) error
 	donec chan struct{}
 	err   error
 }
 
-// Write appends p to w. User must call Close to close the w after done writing.
 func (w *writer) Write(p []byte) (int, error) {
-	if len(p) == 0 {
-		return 0, nil
-	}
-	if w.w == nil {
-		pr, pw := io.Pipe()
-		w.w = pw
-		w.open(pr)
-		return 0, nil
-	}
 	select {
 	case <-w.donec:
 		return 0, w.err
@@ -69,34 +61,19 @@ func (w *writer) Write(p []byte) (int, error) {
 	return w.w.Write(p)
 }
 
-// pr may be nil if we're Closing and no data was written.
-func (w *writer) open(pr *io.PipeReader) {
+func (w *writer) pipe(pr *io.PipeReader, upload func(r io.Reader) error) {
 	go func() {
 		defer close(w.donec)
-		var r io.Reader = http.NoBody
-		if pr != nil {
-			r = pr
-		}
-		err := w.send(r)
+		err := upload(pr)
 		if err != nil {
 			w.err = err
-			if pr != nil {
-				pr.CloseWithError(err)
-			}
+			pr.CloseWithError(err)
 		}
 	}()
 }
 
-// Close completes the writer and closes it. Any error occurring during write
-// will be returned. If a writer is closed before any Write is called, Close
-// will create an empty file at the given key.
 func (w *writer) Close() error {
-	if w.w == nil {
-		// We never got any bytes written. We'll write an http.NoBody.
-		w.open(nil)
-	} else if err := w.w.Close(); err != nil {
-		return err
-	}
+	w.w.Close()
 	<-w.donec
 	return w.err
 }
--- s3/bucket_test.go
+++ s3/bucket_test.go
@@ -1,37 +1,114 @@
 package s3_test
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io/ioutil"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+	awss3 "github.com/aws/aws-sdk-go/service/s3"
 
 	"github.com/stretchr/testify/assert"
 
-	awss3 "github.com/aws/aws-sdk-go/service/s3"
 	"github.com/ryanmiville/aws/s3"
 	"github.com/ryanmiville/aws/s3/s3fakes"
 )
 
-//go:generate counterfeiter io.ReadCloser
-
 func TestNewReader(t *testing.T) {
-	client := &s3fakes.FakeS3{}
-	rc := &s3fakes.FakeReadCloser{}
-	resp := &awss3.GetObjectOutput{Body: rc}
-	client.GetObjectWithContextReturns(resp, nil)
-	b := s3.NewBucket(client, "bucketName")
-	ctx := context.Background()
+	t.Run("gets a reader from S3", func(t *testing.T) {
+		client := &s3fakes.FakeS3{}
+		rc := ioutil.NopCloser(&bytes.Buffer{})
+		resp := &awss3.GetObjectOutput{Body: rc}
+		client.GetObjectWithContextReturns(resp, nil)
+		b := s3.NewBucket(client, "bucketName")
+		ctx := context.Background()
 
-	r, err := b.NewReader(ctx, "path/to/blob")
-	assert.NoError(t, err)
+		r, err := b.NewReader(ctx, "path/to/blob")
+		assert.NoError(t, err)
 
-	assert.Equal(t, rc, r)
+		assert.Equal(t, rc, r)
+	})
+
+	t.Run("client fails", func(t *testing.T) {
+		client := &s3fakes.FakeS3{}
+		expected := errors.New("get object failed")
+		client.GetObjectWithContextReturns(nil, expected)
+		b := s3.NewBucket(client, "bucketName")
+		ctx := context.Background()
+
+		r, err := b.NewReader(ctx, "path/to/blob")
+		assert.True(t, errors.Is(err, expected))
+		assert.Nil(t, r)
+	})
 }
 
 func TestNewWriter(t *testing.T) {
-	client := &s3fakes.FakeS3{}
-	b := s3.NewBucket(client, "bucketName")
-	ctx := context.Background()
+	t.Run("writes to s3", func(t *testing.T) {
+		client := &s3fakes.FakeS3{}
+		var read []byte
+		var readErr error
+		client.UploadWithContextStub = func(_ context.Context, in *s3manager.UploadInput, opts ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+			read, readErr = ioutil.ReadAll(in.Body)
+			return nil, nil
+		}
+		b := s3.NewBucket(client, "bucketName")
+		ctx := context.Background()
 
-	w, err := b.NewWriter(ctx, "path/to/blob")
-	assert.NoError(t, err)
+		w := b.NewWriter(ctx, "path/to/blob")
+
+		n, err := w.Write([]byte("hello "))
+		assert.Equal(t, 6, n)
+		assert.NoError(t, err)
+
+		n, err = w.Write([]byte("world"))
+		assert.Equal(t, 5, n)
+		assert.NoError(t, err)
+
+		err = w.Close()
+		assert.NoError(t, err)
+
+		assert.Equal(t, []byte("hello world"), read)
+		assert.NoError(t, readErr)
+	})
+
+	t.Run("client fails", func(t *testing.T) {
+		client := &s3fakes.FakeS3{}
+		expected := errors.New("upload failed")
+		client.UploadWithContextStub = func(_ context.Context, in *s3manager.UploadInput, opts ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
+			ioutil.ReadAll(in.Body)
+			return nil, expected
+		}
+		b := s3.NewBucket(client, "bucketName")
+		ctx := context.Background()
+
+		w := b.NewWriter(ctx, "path/to/blob")
+
+		n, err := w.Write([]byte("hello "))
+		assert.Equal(t, 6, n)
+		assert.NoError(t, err)
+
+		n, err = w.Write([]byte("world"))
+		assert.Equal(t, 5, n)
+		assert.NoError(t, err)
+
+		err = w.Close()
+		assert.True(t, errors.Is(err, expected))
+	})
+
+	t.Run("Write after Close", func(t *testing.T) {
+		client := &s3fakes.FakeS3{}
+		expected := errors.New("sticky error")
+		client.UploadWithContextReturns(nil, expected)
+		b := s3.NewBucket(client, "bucketName")
+		ctx := context.Background()
+
+		w := b.NewWriter(ctx, "path/to/blob")
+		_ = w.Close()
+		n, err := w.Write([]byte("should not work"))
+		assert.True(t, errors.Is(err, expected))
+		assert.Zero(t, n)
+	})
 }
